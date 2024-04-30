a84f134c934f11758028b0360ccc31dae6046471
VaishooR <vaishooramkrish@gmail.com>
2024-04-30 19:58:26 +0530

    Fetch Api Data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,10 @@
-import swiggyData from "../utils/swiggyData.js";
 import Card from "./Card";
 import {useState,useEffect} from "react";
 
 // BODY
 const Body = () => {
-    const [restaurantList,setrestaurantList] = useState(swiggyData);
+
+    const [restaurantList,setrestaurantList] = useState([]);
 
     const handleAllRestaurants = () => {
         const allRestaurants = swiggyData
@@ -32,7 +32,16 @@ const Body = () => {
         setrestaurantList(filterDeliveryTime)
     }
 
-    useEffect(()=>{},[restaurantList])
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    const fetchData = async () =>{
+        const swiggyApi = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const swiggyApiData = await swiggyApi.json();
+        const restaurantsFromApi = swiggyApiData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        setrestaurantList(restaurantsFromApi);
+    }
     
     return (
         <div className="body">
