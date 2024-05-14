c6b80081d0678b76819d077b9b2428df8d48e0f1
VaishooR <vaishooramkrish@gmail.com>
2024-05-14 17:52:33 +0530

    Added style to Restaurant Card Offer Texts

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,22 @@ import star from '../assets/star.png';
 
 const Card = (props) =>{
     const {data} = props;
-    const {name,avgRating,costForTwo,cloudinaryImageId,areaName,cuisines,totalRatingsString,sla} = data.info
-    const {slaString,lastMileTravelString} = data.info.sla
+    const {name,avgRating,costForTwo,cloudinaryImageId,areaName,cuisines,totalRatingsString,sla} = data?.info
+    const {slaString,lastMileTravelString} = data?.info?.sla
     return (
         
         <div className="card">
-            <img src={`${IMG_URL}${cloudinaryImageId}`}/>
+             
+            <div className="img-offer-container">
+                <img src={`${IMG_URL}${cloudinaryImageId}`}/>
+                {data?.info?.aggregatedDiscountInfoV3 === undefined ? 
+                <p className= "offerText"></p> : 
+                <p className= "offerText">
+                    {data?.info?.aggregatedDiscountInfoV3?.header} {data?.info?.aggregatedDiscountInfoV3?.subHeader}
+                </p> 
+                }                
+            </div>
+            
             <div className="card-text">
                 <p className="card-res-name">{name}</p>
                 <p className="card-rating-cost">
