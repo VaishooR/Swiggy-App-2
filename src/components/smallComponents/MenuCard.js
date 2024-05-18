47849159becfd69eaefbe0c635bed4faadaa1de6
VaishooR <vaishooramkrish@gmail.com>
2024-05-18 17:41:14 +0530

    Add/Remove button in Cart Items

--- src/components/smallComponents/MenuCard.js
+++ src/components/smallComponents/MenuCard.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import AccordionFunc from './Accordion';
 import star from '../../assets/star.png';
-import { useDispatch } from 'react-redux';
-import { addCartItem } from '../../store/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCartItem, removeCartItem } from '../../store/cartSlice';
 
 const MenuCard = ({mainCategories}) => {
 
   const dispatch = useDispatch();
 
+  const cartItems = useSelector(store => store.cart.cartItems);
+
   const handleAddToCart = (item) => {
       dispatch(addCartItem(item))
   }
+
+  const handleRemove = (item) => {
+    dispatch(removeCartItem(item))
+}
 
   return (
     <div className="resMenu-menuCard">
@@ -43,10 +49,11 @@ const MenuCard = ({mainCategories}) => {
                   <img
                     src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${imageId}`}
                     alt="No Image"
-                  />
-                  <div>                  
-                    <button className='menu-img-btn'onClick={()=>handleAddToCart(item.card.info)}>Add</button>
-                  </div>
+                  />                 
+                  {cartItems.some(cartitem=>cartitem.id===item?.card?.info?.id)?
+                                        <button onClick={()=>handleRemove(item.card.info.id)} className="remove-btn">Remove</button> :
+                                        <button onClick={()=>handleAddToCart(item.card.info)} className='menu-img-btn'>Add</button>
+                  }
                 </div>
               </div>
             );
